58d4aa6ce728e1f3f0341c353dcc8687e48152bf
Ray <rmo1@ualberta.ca>
2022-10-20 10:22:04 -0600

    woop

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('service/authors/', include("author.urls")),
     path('service/authors/<str:author_id>/posts/', include("post.urls")),
     path('service/authors/<str:author_id>/posts/<str:post_id>', include("post.urls")),
-    path('service/', include(('backend.routers', 'backend'), namespace='backend-api'))
+    path('service/', include(('backend.routers', 'backend'), namespace='backend-api')),
     path('service/authors/<str:author_id>/posts/<str:post_id>/comments/', include("comments.urls")),
 
 
