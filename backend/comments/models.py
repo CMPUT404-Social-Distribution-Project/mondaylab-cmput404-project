45c2a033f94e18a875560bd4eabe9fb24fd3cfd1
Ray <rmo1@ualberta.ca>
2022-10-25 15:22:04 -0600

    created CommentSrc model

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.utils.timezone import now
 from django.utils import dateparse
 from django.db.models import (Model, CharField, URLField, TextChoices, TextField,
-ForeignKey, CASCADE, IntegerField, DateTimeField,BooleanField, UUIDField
+ForeignKey, CASCADE, IntegerField, DateTimeField,BooleanField, UUIDField, PositiveIntegerField,
+ManyToManyField
 )
 
 from uuid import uuid4
@@ -21,7 +22,7 @@ class Visibility(TextChoices):
     FRIENDS = 'FRIENDS', 'FRIENDS'
 
 class Comment(Model):
-    type =  CharField(blank=False, null=False, default="comment", max_length=200)
+    type = CharField(blank=False, null=False, default="comment", max_length=200)
     id = URLField(blank=True, null=False)
     uuid = UUIDField(primary_key=True, default=uuid4, editable=False)
     # when all row in author is deleted, this comment will be too
@@ -29,6 +30,14 @@ class Comment(Model):
     contentType = CharField(default='text/markdown', blank=False, null=False, max_length=200)
     published = CharField(blank=True, max_length=200)
     comment = CharField(blank=True, null=True,  max_length=200, default="empty comment")
+
+class CommentSrc(Model):
+    type = CharField(blank=False, null=False, default="comments", max_length=200)
+    page = PositiveIntegerField(default=1)
+    size = PositiveIntegerField(default=5)
+    post = URLField(blank=True, null=False)
+    id = URLField(primary_key=True, blank=True, null=False)
+    comments = ManyToManyField(Comment, blank=True)
 
 
 
