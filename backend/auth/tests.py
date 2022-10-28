b359da03a576ffc37de781619a5c04df656de541
Ray <rmo1@ualberta.ca>
2022-10-27 20:13:16 -0600

    Mock authorization works. Veryhacky. Owell :shrug:

--- backend/auth/tests.py
+++ backend/auth/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from author.models import Author
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
-# Create your tests here.
+
+
+class AuthorSignupLoginTestCase(APITestCase):
+    def setUp(self):
+        self.displayName = "test"
+        self.password = "testtest"
+        self.user_info = {'displayName': self.displayName, 'password': self.password}
+        self.client = APIClient()
+        self.refresh = None
+
+    def testRegister(self):
+        # test for password validation i.e. password less than 8 chars
+        data = { 'displayName': self.displayName, 'password': 'nope'}
+        response = self.client.post(f'/service/auth/register/', data , format="json")
+        self.assertEqual(response.status_code, 400)
+        # now actually test registering
+        response = self.client.post(f'/service/auth/register/', self.user_info , format="json")
+        self.assertEqual(response.status_code, 201)
+        res_data = response.data
+        # Check that we get all the expected fields
+        self.assertTrue('token' in res_data.keys())
+        self.assertTrue('refresh' in res_data.keys())
+        self.assertTrue('user' in res_data.keys())
+
+    def testLogin(self):
+        self.testRegister()
+        response = self.client.post(f'/service/auth/login/', self.user_info , format="json")
+        self.assertEqual(response.status_code, 200)
+        # Check that we get all the expected fields
+        self.assertTrue('access' in response.data.keys())
+        self.assertTrue('refresh' in response.data.keys())
+        self.assertTrue('user' in response.data.keys())
+
+
