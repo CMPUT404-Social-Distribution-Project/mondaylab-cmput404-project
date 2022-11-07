47cd3d2ee4240fe91cc9cf71ee76836cf5a0765a
truong <truonggi@ualberta.ca>
2022-11-07 14:07:57 -0700

    gonna start generating username and password for remote

--- backend/node/nodeFunction.py
+++ backend/node/nodeFunction.py
@@ -40,7 +40,6 @@ Custom RestFramework basic authentication by overriding the authenticate() metho
 this class will be  specified under backend.settings.py   under DEFAULT_AUTHENTICATION_CLASSES
 
 
-i want to use this to authenticate incoming node connections
 
 https://stackoverflow.com/questions/32844784/django-rest-framework-custom-authentication
 
@@ -50,15 +49,15 @@ class CustomBasicAuthentication(authentication.BasicAuthentication):
         """
         https://stackoverflow.com/questions/38016684/accessing-username-and-password-in-django-request-header-returns-none
 
-        I WANT TO AUTHENTICATE INCOMING HTTP REQUEST TO GET OUR HOST FROM OTHER SERVER
-
-        WHENEVER USER GET 
+        I WANT TO AUTHENTICATE INCOMING HTTP REQUEST FROM OTHER HOST TO OUR.
         """
+        
+
         if (request.META.get('HTTP_AUTHORIZATION') != None):
         
             auth_header = request.META['HTTP_AUTHORIZATION']  # 'Basic aGVsbG86d29scmQ='
 
-            if ('Basic' not in auth_header):
+            if ('Basic' not in auth_header):  # this is not a basic AUTH
                 return response.Response(f"NO BASIC AUTH PROVIDED ", status=status.HTTP_404_NOT_FOUND)
 
             encodedCredentials = auth_header.split(' ')[1]  # remove the 'Basic' string
@@ -66,26 +65,27 @@ class CustomBasicAuthentication(authentication.BasicAuthentication):
             username = decodedCredentials[0]
             password = decodedCredentials[1]
 
-            if username != "user" and password != "user":
+            if username != "user" and password != "user":  # some hard coded username, password for now
                 raise exceptions.AuthenticationFailed("Node Credentials Incorrect.")
 
-
-
-            # add the server to the connected server list
-            # NOTE, idk if we should make other Host send their 'hostname' throught their get request body orsomething
-            # NOTE, idk what is the purpose of having authUSername, authPassword in the model as we can hardcode
-            # the credential and check it here
-            # NOTE, lets not fetch from Node db everytime we wanna authenticate a host? because that might be slow
-            #newNode = Node(hostName=hostName, authUsername=username, authPassword=password)
-            #newNode.save()
-
-            # 
             # once a host exists in the node database, then they are authenticated
+
+            """
+            we checkif this foreign host is in the database, then that means we are still  connecting to this host
+            
+            
+            """ 
+
+            remoteNode = Node.objects.filter(authUsername=username, authPassword=password)
+            if not remoteNode.exists():
+                raise exceptions.AuthenticationFailed("This host is not connect")
+
             
             return (Authenticated(), None)  # need to return what?
         else:
             None
 """
+custom class that tell rest framework authenticator to please authenticate this request
 rest framework authenticator will check self.is_authenticated to be true. That is the only field that matters
 """
 class Authenticated:
