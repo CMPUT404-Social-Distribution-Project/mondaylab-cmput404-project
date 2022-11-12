1fd4c424b0dcbc56e45efef816ec76d9bf4e73a7
truong <truonggi@ualberta.ca>
2022-11-12 16:34:20 -0700

    fixed img and edit post issues in fronendend caused by CustomBasic AUth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,7 +145,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',  # 1st line of authentication
-        #'node.nodeFunction.CustomBasicAuthentication'  # 2nd line of authentication
+        'node.nodeFunction.CustomBasicAuthentication'  # 2nd line of authentication
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
