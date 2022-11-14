e2738452126e97e30ac540af5837da6d50c9664a
kimberly <kimberlytran33@hotmail.com>
2022-11-14 14:17:51 -0700

    deploying on heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # your static/ files folder
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
---
+++ backend/static/precache-manifest.542657125497d3eb4f1ca9ecbc18e758.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "047a27f02055b1c9eaca",
+    "url": "/static/css/main.9986fd37.chunk.css"
+  },
+  {
+    "revision": "047a27f02055b1c9eaca",
+    "url": "/static/js/main.b847b872.chunk.js"
+  },
+  {
+    "revision": "2299978759ae3fe8fde3",
+    "url": "/static/js/runtime~main.23c8ae18.js"
+  },
+  {
+    "revision": "9f365f6001004c4c57b4",
+    "url": "/static/css/2.23d81626.chunk.css"
+  },
+  {
+    "revision": "9f365f6001004c4c57b4",
+    "url": "/static/js/2.84a5a0c7.chunk.js"
+  },
+  {
+    "revision": "8eb6f900e876a58cc0f3",
+    "url": "/static/js/3.50c1a8a1.chunk.js"
+  },
+  {
+    "revision": "7cd380a651260694118be45f5760e888",
+    "url": "/static/media/logo.7cd380a6.svg"
+  },
+  {
+    "revision": "2a284a73f223b024493e42460fdca634",
+    "url": "/static/media/logo-text.2a284a73.svg"
+  },
+  {
+    "revision": "ece94e04d3ee14910dcc12fb2299bb2d",
+    "url": "/static/media/ReadexPro-Light.ece94e04.ttf"
+  },
+  {
+    "revision": "af6b38d19c3f91542499f54632bc51fc",
+    "url": "/static/media/ReadexPro-Regular.af6b38d1.ttf"
+  },
+  {
+    "revision": "37c649bf3289bf81278cb48f2690bf19",
+    "url": "/static/media/ReadexPro-Bold.37c649bf.ttf"
+  },
+  {
+    "revision": "6adfea6123131607dec4c1c79be8a3d5",
+    "url": "/static/media/ReadexPro-Medium.6adfea61.ttf"
+  },
+  {
+    "revision": "e515be5a399cff9ec24329055cc12661",
+    "url": "/static/media/outline.e515be5a.svg"
+  },
+  {
+    "revision": "1c5b9ff11da5d97f7a1a87a7619d203d",
+    "url": "/static/media/backdrop.1c5b9ff1.png"
+  },
+  {
+    "revision": "34f4e747d1f47e961a8cf3c513c1b1b7",
+    "url": "/index.html"
+  }
+];
