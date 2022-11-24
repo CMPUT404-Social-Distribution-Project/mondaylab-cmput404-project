022b5876886331451c62358e019caeb8d758188c
Ray <rmo1@ualberta.ca>
2022-11-24 16:56:41 -0700

    Make admin panel prettier

--- backend/inbox/admin.py
+++ backend/inbox/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Inbox
 
-# Register your models here.
-admin.site.register(Inbox)
+class InboxAdmin(admin.ModelAdmin):
+    filter_horizontal = ('posts', 'comments', 'likes', 'follow_requests',)
+
+admin.site.register(Inbox, InboxAdmin)
