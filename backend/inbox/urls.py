ddcbb212ae82b8ae1ed1b7b55763d578305ef5d9
Ray <rmo1@ualberta.ca>
2022-11-24 11:49:27 -0700

    Remove end slash on inbox url- team 2 has no slash

--- backend/inbox/urls.py
+++ backend/inbox/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from inbox import views
 
 urlpatterns = [
-    path("", views.InboxApiView.as_view(), name="retrieving an inbox"),
-    path("all/", views.InboxAllApiView.as_view(), name="check if follow display in inbox"),
-    path("<str:foreign_author_id>/", views.InboxDeleteFRApiView.as_view(), name="delete follow request from author ID"),
+    path("/", views.InboxApiView.as_view(), name="retrieving an inbox"),
+    path("/all/", views.InboxAllApiView.as_view(), name="check if follow display in inbox"),
+    path("/<str:foreign_author_id>/", views.InboxDeleteFRApiView.as_view(), name="delete follow request from author ID"),
 
 
 ]
