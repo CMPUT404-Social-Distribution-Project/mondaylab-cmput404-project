0137d5f3324087a17329dda88379482998321cfa
Ray <rmo1@ualberta.ca>
2022-11-24 12:01:11 -0700

    remove slash agne

--- backend/inbox/urls.py
+++ backend/inbox/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from inbox import views
 
 urlpatterns = [
-    path("/", views.InboxApiView.as_view(), name="retrieving an inbox"),
+    path("", views.InboxApiView.as_view(), name="retrieving an inbox"),
     path("/all/", views.InboxAllApiView.as_view(), name="check if follow display in inbox"),
     path("/<str:foreign_author_id>/", views.InboxDeleteFRApiView.as_view(), name="delete follow request from author ID"),
 
