8f597e08c60091e0a624e4d25a770830e311b3f4
Ray <rmo1@ualberta.ca>
2022-11-28 10:00:27 -0700

    Remove prints

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -59,9 +59,7 @@ class CommentsApiView(GenericAPIView):
                 # older comments first/top
                 commentsQuerySet = Comment.objects.filter(id__contains = post_id).order_by("published")
                 commentsPaginateQuerySet = self.paginate_queryset(commentsQuerySet)
-                print(commentsPaginateQuerySet)
                 commentsSerializer = CommentsSerializer(commentsPaginateQuerySet, many=True)
-                print(commentsSerializer.data)
                 comments = commentsSerializer.data
                 # commentsPaginationResult = self.get_paginated_response(commentsSerializer.data)
                 # comments = commentsPaginationResult.data.get("results")
