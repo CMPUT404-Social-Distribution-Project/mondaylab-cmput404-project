55ca3446b102a205f91e5217a69b04d0d4cb220d
Ray <rmo1@ualberta.ca>
2022-12-02 09:40:33 -0700

    Add some debug msgs, fix fetch_authors?

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -491,19 +491,23 @@ def fetch_author(author_uuid):
     '''
 
     uuid_in_id = Author.objects.filter(id__contains=author_uuid)
-    uuid = Author.objects.filter(uuid=author_uuid)
     if uuid_in_id.exists():
         return uuid_in_id.first()
-    elif uuid.exists():
-        return uuid.first()
+    if isUUID(author_uuid):
+        uuid = Author.objects.filter(uuid=author_uuid)
+        if uuid.exists():
+            return uuid.first()
 
     nodes = Node.objects.all()
     for node in nodes:
         authors_url = f"{node.host}authors/{author_uuid}"
         res = authenticated_GET(authors_url, node)
         if res.status_code == 200:
+            print(f"fetch_author: Found author at {authors_url}!",res.content)
             create_remote_author(res.json())
             return Author.objects.get(id__contains=author_uuid)
+        else:
+            print(f"Failed to check if author exists at {authors_url}.")
 
     return "Author was not found"
 
