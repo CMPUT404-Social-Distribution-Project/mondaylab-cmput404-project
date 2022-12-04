e5484997733c1cab1dac2f2a9ceb7ac1910def9e
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-03 23:50:34 -0700

    fix test by add oneline code

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -17,7 +17,7 @@ from node.utils import authenticated_GET, authenticated_POST, authenticated_GET_
 from uuid import uuid4
 
 our_frontends = ["http://localhost:3000"]
-our_backends = ["http://localhost:8000"]  # TODO, add the heroku host origin here too
+our_backends = ["http://localhost:8000", "http://testserver"]  # TODO, add the heroku host origin here too
 author_required_fields = ["type", "id", "url", "host", "displayName", "github", "profileImage"]
 
 
