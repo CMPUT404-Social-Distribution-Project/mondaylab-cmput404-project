b4924a9e55fcc3dd25fdd8fc8692569240bfa9c7
jackie174 <57372321+jackie174@users.noreply.github.com>
2022-12-04 17:51:10 -0700

    Delete index.js

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-import { join } from 'path'
-
-// Serve static files from the React frontend app
-app.use(express.static(join(__dirname, '../socialapp/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname + '/../socialapp/build/index.html'))
-})
